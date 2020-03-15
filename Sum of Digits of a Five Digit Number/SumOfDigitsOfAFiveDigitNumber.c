49f2677e313fd7a20e936b81abcb56495b1f76dd
JMJ Riyana <jeemariyana@gmail.com>
2020-03-15 10:52:51 +0530

    Conditionals and Loops Solutions

---
+++ Sum of Digits of a Five Digit Number/SumOfDigitsOfAFiveDigitNumber.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+	
+    int n,sum;
+    scanf("%d", &n);
+    sum=n%10+(n%100)/10+((n%1000)/100)+(n%10000)/1000+n/10000;
+    printf("%d",sum);
+    return 0;
+}
+
